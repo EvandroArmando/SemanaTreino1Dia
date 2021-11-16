0d130ab2673549a1a4c1401c51231b8fd5b3d41e
Abel Domingos <70151385+abeldomingos@users.noreply.github.com>
2021-11-17 00:51:48 +0100

    adicionei new things

--- src/semanatreino1dia/classes/Loja.java
+++ src/semanatreino1dia/classes/Loja.java
@@ -10,5 +10,30 @@ package semanatreino1dia.classes;
  * @author Tequerro
  */
 public class Loja {
+    String dono;
+    String nome_loja;
+
+    public Loja(String dono, String nome_loja) {
+        this.dono = dono;
+        this.nome_loja = nome_loja;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public String getNome_loja() {
+        return nome_loja;
+    }
+
+    public void setNome_loja(String nome_loja) {
+        this.nome_loja = nome_loja;
+    }
+    
+    
     
 }
