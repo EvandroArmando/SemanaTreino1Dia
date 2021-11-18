a054651809c52fde999feefce7fb3a9796ed4e09
EvandroArmando <53578532+EvandroArmando@users.noreply.github.com>
2021-11-18 13:13:10 +0100

    reconfigurando

--- src/semanatreino1dia/SemanaTreino1Dia.java
+++ src/semanatreino1dia/SemanaTreino1Dia.java
@@ -6,6 +6,7 @@
 package semanatreino1dia;
 
 import semanatreino1dia.classes.Cliente;
+import semanatreino1dia.classes.Funcionarios;
 import semanatreino1dia.classes.Usuario;
 
 /**
@@ -21,11 +22,18 @@ public class SemanaTreino1Dia {
         // TODO code application logic here
         
         
-        
+        //instaciamento das classes
         Cliente cliente [] = new Cliente[6];
         Usuario  usuario [] = new Usuario[6];
-        usuario[0]= new Usuario("adm", "evandro", 18);
-        cliente[0]= new Cliente("kilamba", "masculino", usuario[0]);
+        Funcionarios funcionario []= new Funcionarios[6];
+        
+        //setar as classes
+        usuario[0]= new Usuario("Chefe", "evandro", 18);
+        usuario[2]= new Usuario("Cliente", "Irineia Lukombo", 18);
+        //lojas
+        usuario[1]= new Usuario("Funcionario", "Abel", 18);
+        funcionario[0]= new Funcionarios("kilamba", "Atendente de caixa",usuario[0], usuario[0].getNome(), usuario[1].getNome(), usuario[0].getIdade());  
+        cliente[0]= new Cliente("kilamba", "masculino", usuario[2]);
         
         
         
--- src/semanatreino1dia/classes/Funcionarios.java
+++ src/semanatreino1dia/classes/Funcionarios.java
@@ -23,6 +23,10 @@ public class Funcionarios extends Usuario {
     }
 
     
+    
+    
+    
+    
     public String getMorada() {
         return morada;
     }
